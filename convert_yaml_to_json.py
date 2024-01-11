cc91e9605b6dc754939da0454adff3dcaf467c12
jtsdev2023 <jtsdev2023@gmail.com>
2024-01-11 18:48:11 -0500

    updates

--- convert_yaml_to_json.py
+++ convert_yaml_to_json.py
@@ -6,7 +6,7 @@ import yaml
 
 def read_yaml_file() -> Tuple[int, str]:
     """DOC STRING"""
-    yaml_file = 'web-layout.yaml'
+    yaml_file = 'web-page-layout.yaml'
     directory_content_list = os.listdir()
 
     if yaml_file in directory_content_list:
@@ -18,7 +18,7 @@ def read_yaml_file() -> Tuple[int, str]:
             print(error)
             return (1, error)
     else:
-        error_msg = f'File {yaml_file} does not exist.'
+        error_msg = f'File: "{yaml_file}" does not exist.'
         print(error_msg)
         return (1, error_msg)
 
@@ -43,14 +43,30 @@ def convert_yaml_to_json(yaml_input_object: list) -> Tuple[int, str]:
         return (1, error)
 
 
+def write_json_to_file(input_json_string: str) -> str:
+    """DOC STRING"""
+    json_file = 'web-layout.json'
+    success_msg = f'Content written to file "{json_file}"'
+    fail_msg = f'Error in writting to file "{json_file}"'
+
+    try:
+        with open(json_file, 'w', encoding='utf-8') as f:
+            f.write(input_json_string)
+        return (0, success_msg)
+    except Exception as error:
+        print(error)
+        return (1, fail_msg)
+
+
 def main():
     """DOC STRING"""
     error, yaml_string = read_yaml_file()
     if error == 0:
         try:
-            yaml_object = generate_yaml_obj(yaml_string)
-            json_string = convert_yaml_to_json(yaml_object)
-            return (0, json_string)
+            _, yaml_object = generate_yaml_obj(yaml_string)
+            _, json_string = convert_yaml_to_json(yaml_object)
+            _, ops_msg = write_json_to_file(json_string)
+            return (0, ops_msg)
         except Exception as error:
             print(error)
             return (1, error)
